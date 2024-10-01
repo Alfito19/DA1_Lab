37a3e00adae665421247b12b72d2e77ce3cb48a7
Alfonso Saizar <alfo.saizar19@gmail.com>
2024-10-01 17:28:02 -0300

    Program automotive

--- Automotive/Program/Program.cs
+++ Automotive/Program/Program.cs
@@ -1,5 +1,18 @@
-﻿namespace Program;
+﻿using Automotive;
 
-public class Program
+var car = new Car(5,"red");
+var car2 = new Car(3,"purple");
+var car3 = new Car(4,"green");
+var truck = new Truck(5,"red");
+var truck2 = new Truck(3,"purple");
+var truck3 = new Truck(4,"green");
+
+List<Vehicle> vehicles = new List<Vehicle>()
 {
+    car,car2,car3,truck,truck2,truck3
+};
+
+foreach (var v in vehicles)
+{
+    v.TurnOn();
 }
