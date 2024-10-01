248965e55e1b3be1c1ab1b76e98ecba14188120f
Alfonso Saizar <alfo.saizar19@gmail.com>
2024-10-01 19:35:18 -0300

    vehicle test update

--- Automotive/Automotive/Vehicle.cs
+++ Automotive/Automotive/Vehicle.cs
@@ -3,6 +3,7 @@
 public class Vehicle
 {
     private int _doorQuantity;
+    public int Year;
     public int DoorQuantity { get=>_doorQuantity;
         set
         {
@@ -13,20 +14,35 @@ public class Vehicle
             _doorQuantity = value;
         } }
     public string Color { get; set; }
+    
 
     public string Info
     {
         get => $"Color {Color}, Door {DoorQuantity}";
     }
 
-    public Vehicle(int doorQuantity, string color)
+    public Vehicle(int doorQuantity, string color, int year = 2024)
     {
         DoorQuantity = doorQuantity;
         Color = color;
+        Year = year;
     }
 
     public virtual void TurnOn()
     {
         Console.WriteLine("Remember to override this method to print the correct information");
+    }
+    
+    public double CalculatePrice(float pricePerDoor, double depreciation)
+    {
+        var total = DoorQuantity*pricePerDoor;
+        var yearNow = DateTime.Now.Year;
+        var discount = (yearNow - Year)*depreciation*total;
+
+        if (total - discount <= 0)
+        {
+            return total * 0.1;
+        }
+        return total-discount;
     }
 }
--- Automotive/AutomotiveTest/VehicleTest.cs
+++ Automotive/AutomotiveTest/VehicleTest.cs
@@ -3,6 +3,16 @@ namespace AutomotiveTest;
 [TestClass]
 public class VehicleTest
 {
+    private double Depreciation;
+    private float PricePerDoor;
+
+    [TestInitialize]
+    public void InitializeAttributes()
+    {
+        this.Depreciation = 0.1;
+        this.PricePerDoor = 10;
+    }
+    
     [TestMethod]
     public void DoorQuantitySet_ValidDoors()
     {
@@ -13,7 +23,7 @@ public class VehicleTest
         //assert
         Assert.AreEqual(5, v.DoorQuantity);
     }
-    
+
     [TestMethod]
     [ExpectedException(typeof(ArgumentException))]
     public void DoorQuantitySet_InvalidLessThan0Doors()
@@ -22,6 +32,36 @@ public class VehicleTest
         Vehicle v;
         //act
         v = new Vehicle(-4, "red");
-        //assert
     }
+
+    [TestMethod]
+    public void DoorQuantitySet_Info()
+    {
+        Vehicle v;
+        v = new Vehicle(5, "red");
+        Assert.AreEqual("Color red, Door 5", v.Info);
+    }
+
+    [TestMethod]
+    public void CalculatePrice_Successfully()
+    {
+        //arrange
+        Vehicle v = new Vehicle(5, "blue");
+        //act
+        var price = v.CalculatePrice(PricePerDoor,Depreciation);
+        //assert
+        Assert.AreEqual(50, price);
+    }
+
+    [TestMethod]
+    public void CalculatePrice_Base_Price()
+    {
+        //arrange
+        Vehicle v = new Vehicle(5, "blue",1980);
+        //act
+        var price = v.CalculatePrice(PricePerDoor,Depreciation);
+        //assert
+        Assert.AreEqual(5, price);
+    }
+
 }
