75218f77ed5b41729325b17db6013f73dfbf62b4
Alfonso Saizar <alfo.saizar19@gmail.com>
2024-10-01 17:18:42 -0300

    Update Vehicle.cs

--- Automotive/Automotive/Vehicle.cs
+++ Automotive/Automotive/Vehicle.cs
@@ -2,5 +2,18 @@ namespace Automotive;
 
 public class Vehicle
 {
-    
+    public int DoorQuantity { get; set; }
+    public string Color { get; set; }
+
+    public Vehicle(int doorQuantity, string color)
+    {
+        DoorQuantity = doorQuantity;
+        Color = color;
+    }
+
+    public virtual void TurnOn()
+    {
+        Console.WriteLine("Remember to override this method to print the correct information");
+    }
+
 }
